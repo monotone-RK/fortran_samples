97985da6023b21ce5757150947f4fde78fe0ec1b
monotone-RK <kobaya4121@gmail.com>
2023-05-11 19:32:08 +0900

    1CPUで動作するサンプルコードを追加

---
+++ hello/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#------- qsub option -----------
+#PBS -A CCUSC
+#PBS -q gpu
+#PBS -l elapstim_req=00:00:10
+
+#------- Program execution ----------
+cd $PBS_O_WORKDIR
+./hello_world.exe
